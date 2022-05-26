017dfdefad5619d4bdf77ed8bf85a8b813d2aed8
asifc <asifc@MyLaptop>
2022-05-26 16:44:55 +0800

    Home Page

--- DSAssignment/src/Asif/App.java
+++ DSAssignment/src/Asif/App.java
@@ -1,20 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package Asif;
 
-/**
- *
- * @author asifc
- */
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 public class App {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Setup application start time
+        long startTime = 0;
+        startTime = System.nanoTime();
+        
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        // Home Page
+        System.out.println("Welcome to Customer always right E-hailing Application!");
+        System.out.println("Options :");
+        System.out.println("Current time : " + TimeElapsed(startTime));
+        
+        // Ask for user input
+        Scanner sc = new Scanner(System.in);
+        String s = "";
+        
+        while (true) {
+            System.out.println("A - View System Dashboad");
+            System.out.println("B - Enter Customer View");
+            System.out.println("C - Add / Remove Driver\n");
+            System.out.print(">> ");
+            s = sc.next();
+            if (s.equals("A") || s.equals("B") || s.equals("C"))
+                break;
+        }
+        
+        if (s.equals("A")) {
+            System.out.println("System dashboard");
+        }
+        else if (s.equals("B")) {
+            System.out.println("Customer View");
+        }
+        else {
+            System.out.println("Driver overview");
+        }
     }
     
+    public static long TimeElapsed(long startTime) {
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        
+        return elapsedTime;
+    }
 }
